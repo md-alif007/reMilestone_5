dacd8fcd13e926aba37456eba56230675ed8ede6
md-alif007 <mohammadalif043@gmail.com>
2025-09-28 21:50:02 +0600

    26-4

--- EventListener/js/events.js
+++ EventListener/js/events.js
@@ -9,3 +9,10 @@ const btnMakeyellow = document.getElementById('btn-make-yellow');
 btnMakeyellow.onclick = function makeYellow() {
     document.body.style.backgroundColor = 'yellow';
 }
+
+
+// option 4 for event handling
+document.getElementById('btn-make-green')
+    .addEventListener('click', function () {
+        document.body.style.backgroundColor = 'green';
+    })
