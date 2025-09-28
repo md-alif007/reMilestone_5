1131ab55217ffc3438e66427456c16e6c38e0447
md-alif007 <mohammadalif043@gmail.com>
2025-09-28 22:01:08 +0600

    26-4

---
+++ EventListener/eventHandler/js/handler.js
@@ -0,0 +1,7 @@
+document.getElementById('update-btn')
+    .addEventListener('click', function () {
+        const testEvent = document.getElementById('test');
+
+        testEvent.innerText = 'event updated';
+
+    })
