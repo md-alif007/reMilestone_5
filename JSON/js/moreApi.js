d52bf0908ac3d336e6addcded4a5531706c2a425
md-alif007 <mohammadalif043@gmail.com>
2025-10-05 12:13:57 +0600

    new way to fetch

--- JSON/js/moreApi.js
+++ JSON/js/moreApi.js
@@ -1,5 +1,12 @@
 const loadUserButton = () => {
     fetch ("https://jsonplaceholder.typicode.com/users")
     .then (res => res.json())
-    .then (data => console.log(data));
+    .then (data => {
+        console.log(data);
+        displayUser(data);
+    })
+}
+
+const displayUser = (users) => {
+    console.log(users[0]);
 }
